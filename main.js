46073cc8e6936635622d7d83b34651f542a40db5
Naji Mammeri <naji@pascience.net>
2016-08-06 07:13:26 +0200

    light code reordering

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-let renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.querySelector("#rendererContainer").appendChild(renderer.domElement)
-
 let rgb_for_color = {
   red: 0xff0000,
   green: 0x00ff00,
@@ -10,10 +6,16 @@ let rgb_for_color = {
 }
 let piece_dimension = [0.4, 0.8, 0.1]
 
-let scene = new THREE.Scene()
-let camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 50)
-{ // setup the scene
-  { // render all existing pieces
+function three_setup(parentElementSelector) {
+  let renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.querySelector(parentElementSelector).appendChild(renderer.domElement)
+
+  let scene = new THREE.Scene()
+  let camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 50)
+
+  let pieces = {}
+  {
     let cumulative_color_offset = 0
     let piece_element_geometry = new THREE.BoxGeometry(...piece_dimension);
     ["red" , "green", "blue", "yellow"].forEach((color_name, color_number) => {
@@ -27,6 +29,7 @@ let camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHei
         scene.add(colored_piece_node)
 
         let piece_model = puzzle_models[piece_id]
+
         for(let cell = 0; cell < piece_model.length; cell += 1) {
           let height = piece_model[cell]
           if(height < 1) {
@@ -38,15 +41,22 @@ let camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHei
             element.position.y = piece_dimension[1]*Math.floor(cell / 2)
             element.position.z = piece_dimension[2]*cur_height
             colored_piece_node.add(element)
+            pieces[color_name+piece_number_for_this_color] = colored_piece_node
           }
         }
       })
       cumulative_color_offset += number_of_pieces_for_color(color_name) + 0.5
     })
   }
+
+  return {
+    scene, camera, renderer,
+    three_draw: () => { renderer.render(scene, camera) },
+    pieces
+  }
 }
 
-function draw_frame({ dt, state }) {
+function three_update({ rdr: { camera, three_draw }, dt, state }) {
   {
     textbox.clear()
     let pos = state.camera.position.map(x => Math.floor(x*100)/100)
@@ -60,14 +70,15 @@ function draw_frame({ dt, state }) {
   camera.position.y = state.camera.position[1]
   camera.position.z = state.camera.position[2]
 
-  renderer.render(scene, camera)
+  three_draw() 
 }
 
+let rdr = three_setup("#rendererContainer")
 let state = world_setup()
 let each_frame = (dt) => {
   state = world_step({ dt, state, keys: ngl.pressed_keys, zoomDelta: ngl.mouseWheelDeltaY })
   ngl.mouseWheelDeltaY = 0
-  draw_frame({ dt, state })
+  three_update({ rdr, dt, state })
   requestAnimationFrame(each_frame)
 }
 requestAnimationFrame(each_frame)
